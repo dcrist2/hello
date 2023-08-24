59fc4af48fdd9d3454f32a1f0401aca4dcf5984f
David <dcrist2@huskers.unl.edu>
2023-08-24 12:42:26 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/*
+* Author: David Crist
+*
+* Date: 2023-08-24
+*
+* A program that prints my name
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    printf("My name is David!\n");
+
+    return 0;
+
+}
