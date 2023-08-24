d3b28126083a3c2a62f907a92f64540f0393c03d
pkorlacki <pkorlacki>
2023-08-24 13:45:57 -0500

    added my information

--- hello.c
+++ hello.c
@@ -13,6 +13,8 @@ int main(int argc, char **argv) {
 
     printf("My name is David.\n");
     printf("I am a Computer Engineering major.\n");
+    printf("My name is Piotr.\n");
+    printf("My major is undeclared engineering.\n");
 
     return 0;
 
